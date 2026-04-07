18e53bb8a2681e17df33620fc360e39b40702682
YashIIT0909 <24je0721@iitism.ac.in>
2026-04-08 00:14:07 +0530

    Add support for alarm and shared memory in process management
    - Implemented sys_sigalarm and sys_sigreturn for process alarms.
    - Added alarm-related fields in the proc structure.
    - Introduced sys_shm_get for shared memory allocation and management.
    - Updated trap handling to support alarm functionality.

--- G23_Project1_xv6CustomizeSystemCalls/xv6-riscv/kernel/sysproc.c
+++ G23_Project1_xv6CustomizeSystemCalls/xv6-riscv/kernel/sysproc.c
@@ -13,7 +13,7 @@ sys_exit(void)
   int n;
   argint(0, &n);
   kexit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -47,17 +47,21 @@ sys_sbrk(void)
   argint(1, &t);
   addr = myproc()->sz;
 
-  if(t == SBRK_EAGER || n < 0) {
-    if(growproc(n) < 0) {
+  if (t == SBRK_EAGER || n < 0)
+  {
+    if (growproc(n) < 0)
+    {
       return -1;
     }
-  } else {
+  }
+  else
+  {
     // Lazily allocate memory for this process: increase its memory
     // size but don't allocate memory. If the processes uses the
     // memory, vmfault() will allocate it.
-    if(addr + n < addr)
+    if (addr + n < addr)
       return -1;
-    if(addr + n > TRAPFRAME)
+    if (addr + n > TRAPFRAME)
       return -1;
     myproc()->sz += n;
   }
@@ -71,12 +75,14 @@ sys_pause(void)
   uint ticks0;
 
   argint(0, &n);
-  if(n < 0)
+  if (n < 0)
     n = 0;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
+  while (ticks - ticks0 < n)
+  {
+    if (killed(myproc()))
+    {
       release(&tickslock);
       return -1;
     }
@@ -113,4 +119,93 @@ sys_halt(void)
 {
   *(volatile uint32 *)0x100000 = 0x5555;
   return 0;
+}
+
+uint64
+sys_sigalarm(void)
+{
+  int n;
+  uint64 fn;
+
+  argint(0, &n);
+  argaddr(1, &fn);
+
+  if (n < 0)
+    return -1;
+
+  struct proc *p = myproc();
+  p->alarm_interval = n;
+  p->alarm_handler = (void (*)())fn;
+  p->alarm_ticks = 0;
+
+  return 0;
+}
+
+uint64
+sys_sigreturn(void)
+{
+  struct proc *p = myproc();
+  memmove(p->trapframe, p->alarm_tf, sizeof(struct trapframe));
+  p->alarm_is_running = 0;
+  return p->trapframe->a0;
+}
+
+extern struct spinlock shm_lock;
+extern struct
+{
+  int key;
+  uint64 pa;
+} shm_table[8];
+
+uint64
+sys_shm_get(void)
+{
+  int key;
+  argint(0, &key);
+
+  uint64 pa = 0;
+  acquire(&shm_lock);
+  for (int i = 0; i < 8; i++)
+  {
+    if (shm_table[i].key == key && shm_table[i].pa != 0)
+    {
+      pa = shm_table[i].pa;
+      break;
+    }
+  }
+
+  if (pa == 0)
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      if (shm_table[i].pa == 0)
+      { // empty slot
+        shm_table[i].key = key;
+        shm_table[i].pa = (uint64)kalloc();
+        if (shm_table[i].pa == 0)
+        {
+          release(&shm_lock);
+          return -1;
+        }
+        memset((void *)shm_table[i].pa, 0, PGSIZE);
+        pa = shm_table[i].pa;
+        break;
+      }
+    }
+  }
+  release(&shm_lock);
+
+  if (pa == 0)
+    return -1; // no space
+
+  struct proc *p = myproc();
+  uint64 va = PGROUNDUP(p->sz);
+
+  if (mappages(p->pagetable, va, PGSIZE, pa, PTE_W | PTE_R | PTE_U) != 0)
+  {
+    return -1;
+  }
+
+  p->sz = va + PGSIZE;
+  return va;
 }
